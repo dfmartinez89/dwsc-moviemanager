fe75689ea6cada42f78840fb4475ad278656c33f
thywillbedone <dfmartinez8909@gmail.com>
2022-05-21 19:45:04 +0200

    first commit

---
+++ src/test/java/dwsc/proyecto/moviemanager/MoviemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dwsc.proyecto.moviemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
