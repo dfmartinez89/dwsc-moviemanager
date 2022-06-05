2ecfa221191195a970282dd4e6d3d6eee531906c
thywillbedone <dfmartinez8909@gmail.com>
2022-06-05 12:52:03 +0200

    rm unused imports

--- src/main/java/dwsc/proyecto/moviemanager/controller/MovieManagerController.java
+++ src/main/java/dwsc/proyecto/moviemanager/controller/MovieManagerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import dwsc.proyecto.moviemanager.domain.Comment;
 import dwsc.proyecto.moviemanager.domain.Movie;
